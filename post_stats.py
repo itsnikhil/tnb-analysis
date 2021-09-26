141b5f593854a77587166bd56b5b5a616875b0f0
Nikhil Taneja <taneja.nikhil03@gmail.com>
2021-09-27 00:28:32 +0530

    fixed JSON serialization

--- post_stats.py
+++ post_stats.py
@@ -9,23 +9,23 @@ url = 'http://bank.tnbexplorer.com/stats/api/'
 
 def send(data):
     payload = json.dumps({
-        'shift': data['Shift'],
-        'total': data['Total'],
-        'accounts': data['Accounts'],
-        'max_balance': data['Max balance'],
+        'shift': int(data['Shift']),
+        'total': int(data['Total']),
+        'accounts': int(data['Accounts']),
+        'max_balance': int(data['Max balance']),
         'richest': data['Richest'],
-        'top_5_wealth': data['Top 5% wealth'],
-        'top_5_ownership': data['Top 5% ownership'],
-        'top_5_accounts': data['Top 5% accounts'],
-        'top_10_wealth': data['Top 10% wealth'],
-        'top_10_ownership': data['Top 10% ownership'],
-        'top_10_accounts': data['Top 10% accounts'],
-        'top_25_wealth': data['Top 25% wealth'],
-        'top_25_ownership': data['Top 25% ownership'],
-        'top_25_accounts': data['Top 25% accounts'],
-        'top_50_wealth': data['Top 50% wealth'],
-        'top_50_ownership': data['Top 50% ownership'],
-        'top_50_accounts': data['Top 50% accounts']
+        'top_5_wealth': int(data['Top 5% wealth']),
+        'top_5_ownership': float(data['Top 5% ownership']),
+        'top_5_accounts': int(data['Top 5% accounts']),
+        'top_10_wealth': int(data['Top 10% wealth']),
+        'top_10_ownership': float(data['Top 10% ownership']),
+        'top_10_accounts': int(data['Top 10% accounts']),
+        'top_25_wealth': int(data['Top 25% wealth']),
+        'top_25_ownership': float(data['Top 25% ownership']),
+        'top_25_accounts': int(data['Top 25% accounts']),
+        'top_50_wealth': int(data['Top 50% wealth']),
+        'top_50_ownership': float(data['Top 50% ownership']),
+        'top_50_accounts': int(data['Top 50% accounts'])
     })
 
     headers = {
